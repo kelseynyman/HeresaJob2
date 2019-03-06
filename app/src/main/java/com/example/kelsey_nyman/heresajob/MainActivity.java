a6cbbe9dbb6f0f21b76145cc9b0e61b584649cc7
kelsey-nyman <kelsey01nyman@gmail.com>
2019-03-06 10:10:55 -0500

    3/6/19

--- app/src/main/java/com/example/kelsey_nyman/heresajob/MainActivity.java
+++ app/src/main/java/com/example/kelsey_nyman/heresajob/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.kelsey_nyman.heresajob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailText;
+    private EditText numberText;
+    private EditText addressText;
+    private Button nextButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailText = findViewById(R.id.emailText);
+        numberText = findViewById(R.id.numberText);
+        addressText = findViewById(R.id.addressText);
+        nextButton = findViewById(R.id.nextButton);
     }
 }
